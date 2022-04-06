376a4a632e800302bebcdf714e0610dd0c4a803a
Tam <89226497+tpminhtam@users.noreply.github.com>
2022-04-05 19:11:56 -0700

    wip

--- client/src/components/QuestionAnswer.jsx
+++ client/src/components/QuestionAnswer.jsx
@@ -3,25 +3,51 @@ import React from 'react';
 export default class QuestionAnswer extends React.Component {
     constructor(props) {
         super(props);
-        this,state = {
-            
-                }
+        this.state = {
+            qas: []
+        }
     }
 
     componentDidMount() {
         fetch(`/qa/questions?product_id=${this.props.productId}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             this.setState({
-                qa: data.results
+                qas: data.results.sort((a, b) => {
+                    return b.question_helpfulness - a.question_helpfulness
+                })
             })
         });
       }
 
     render() {
         return (
-            <div>hello</div>
+            <div>
+            {this.state.qas.map((qa) => 
+                <>
+                    <div className='question'>
+                        Q: {qa.question_body}
+                    </div>
+                    <div>
+                        {Object.values(qa.answers).sort((a, b) => {
+                            if (a.answerer_name === 'Seller') {
+                                return -1;
+                            } else if (b.answerer_name === 'Seller') {
+                                return 1;
+                            }
+
+                            return 0;
+                        })
+                        .map(({ body, date, helpfulness, photos }) => (
+                            <div>
+                                A: {body}
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+            </div>
+            
         )
     }
 }
