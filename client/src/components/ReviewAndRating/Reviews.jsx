861e90e54c63c90d75efef94dcf65e79e20a4554
Xinxin Li <xinxinli510@gmail.com>
2022-04-07 13:25:11 -0500

    allow users to vote or report for a review

--- client/src/components/ReviewAndRating/Reviews.jsx
+++ client/src/components/ReviewAndRating/Reviews.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
-import {FaCheck, FaPlus} from "react-icons/fa";
+import styled from 'styled-components';
+import Review from './Review.jsx';
+import SortMenu from './SortMenu.jsx';
+import {FaPlus} from "react-icons/fa";
 
+const Container = styled.div`
+  padding-left:60px;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: 2px solid #d4d4d4;
+  color: #525252;
+  padding: 15px 30px;
+  margin-right: 40px;
+`;
+
+const ReviewList = styled.div`
+  width: 800px;
+  max-height: 600px;
+  overflow: scroll;
+  border: 1px solid #ebebeb;
+  margin-bottom: 20px;
+  padding: 40px;
+`
 
 export default class Reviews extends React.Component {
   constructor(props) {
@@ -8,36 +31,16 @@ export default class Reviews extends React.Component {
   }
 
   render() {
-    const {reviews, sort} = this.props;
+    const {reviews, sort, moreReviewBtn, handleMoreReviewBtnClick, handleSortOptionChange, voteForReview} = this.props;
     return (
-      <div>
-        <h3>{reviews.length} reviews{reviews.length ? `, sorted by ${sort}` : '.'}</h3>
-        {reviews.length && reviews.map(review => {
-          const {review_id, reviewer_name, summary, body, date, helpfulness, photos, rating, recommend, response} = review;
-          const dateObj = new Date(date);
-          var options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-          };
-          var formatedDate = dateObj.toLocaleDateString('en-US', options);
-          return (
-            <div key={review_id}>
-              <div>
-                <h4>rating: {rating}</h4>
-                <h4>{reviewer_name}, {formatedDate}</h4>
-              </div>
-              <h3>{summary}</h3>
-              <p>{body}</p>
-              {recommend? <h4><FaCheck /> I recommend this product</h4> : ''}
-              {response ? <div><h4>Response</h4><p>{response}</p></div>: ''}
-              <h4>Helpful? <a>Yes</a> ({helpfulness}) | <a>Report</a></h4>
-            </div>
-          )
-        })}
-        <button type="button">MORE REVIEWS</button>
-        <button type="button">ADD A REVIEW <FaPlus /></button>
-      </div>
+      <Container>
+        <h3>{reviews.length} reviews, sorted by {reviews.length ? <SortMenu sort={sort} handleSortOptionChange={handleSortOptionChange}/> : '.'}</h3>
+        <ReviewList>
+          {reviews.length && reviews.map((review, index) => <Review review={review} key={index} voteForReview={voteForReview}/> )}
+        </ReviewList>
+        {moreReviewBtn && <Button type="button" onClick={handleMoreReviewBtnClick}>MORE REVIEWS</Button>}
+        <Button type="button">ADD A REVIEW <FaPlus /></Button>
+      </Container>
     );
   }
 }
