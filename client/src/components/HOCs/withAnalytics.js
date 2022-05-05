a4c32385d276f2064b5b001ff052c71b83b18067
Xinxin Li <xinxinli510@gmail.com>
2022-05-05 10:59:39 -0500

    update withAnalytics function

--- client/src/components/HOCs/withAnalytics.js
+++ client/src/components/HOCs/withAnalytics.js
@@ -5,18 +5,36 @@ export const withAnalytics = (Component, widgetName) =>
         sendAnalytics(e) {
             console.log('e.target', e.target);
             console.log('widgetName', widgetName);
+            console.log('tag-name', e.target.tagName);
             var element = '';
-            if (e.target.className) {
-                var seperatorIndex = e.target.className.indexOf('__');
+            if (e.target.tagName === 'svg' || e.target.tagName === 'path') {
+                element = 'icon image';
+            }
+
+
+            if (!element && e.target.className) {
+                var seperatorIndex = e.target.className.indexOf('-');
                 element = e.target.className.slice(0, seperatorIndex);
                 console.log('e.target.className', element);
-            } else {
+            }
+
+            if (!element && e.target.tagName === 'INPUT') {
+                element = `${e.target.tagName}-${e.target.name}-${e.target.value}`;
+            }
+
+            if (!element) {
                 e.target.childNodes.forEach(node => {
-                    console.log('nodeValue', node.nodeValue);
-                    element += node.nodeValue
+                    if (node && typeof node.nodeValue === 'string') {
+                        console.log('node', typeof node);
+                        console.log('nodeValue', typeof node.nodeValue);
+                        element += node.nodeValue
+                    }
                 });
                 console.log('childNodes', element);
             }
+
+            console.log('final element', element);
+
             fetch('/interactions', {
                 method: "POST",
                 headers: {
