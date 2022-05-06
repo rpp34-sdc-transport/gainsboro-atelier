cfbdfc588b1bb7c7bbc8aae833a15079f89910a6
Tam <89226497+tpminhtam@users.noreply.github.com>
2022-05-05 22:40:11 -0700

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
 const path = require('path');
-module.exports = {
-  entry: './client/src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-    ]
-  },
-  devtool : 'inline-source-map',
-  devServer: {
-    historyApiFallback: true,
-  },
-  output: {
-    path: path.join(__dirname, 'client/dist'),
-    filename: 'bundle.js'
+
+module.exports = (_env, argv) => {
+  const mode = argv.mode;
+
+  return {
+    entry: './client/src/index.js',
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+              'postcss-loader'
+          ]
+        },
+      ]
+    },
+    // devtool: 'inline-source-map',
+    devtool: mode === 'production' ? false : 'inline-source-map',
+    devServer: {
+      historyApiFallback: true,
+    },
+    output: {
+      path: path.join(__dirname, 'client/dist'),
+      filename: 'bundle.js'
+    },
   }
 }
